71c7ba44542e2247f3fca7f20e4f510ca92269c8
Gabriel Spacki <ifprgabriel@gmail.com>
2024-04-04 15:15:34 -0300

    developing custom queries

--- src/main/java/com/letroca/repositories/BookRepository.java
+++ src/main/java/com/letroca/repositories/BookRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, String> {
     Optional<Book> findByIsbnAndUserId(String isbn, String userId);
+
+    @Query("SELECT b FROM books WHERE b.user_id = :userId AND b.genre = :genre")
+    List<Book> findByUserIdAndGenre(String userId, String genre);
 }
